24a60967eda4ae08a4455bad4f5914832669ce61
Bob Smith <wasadmin@webagesolutions.com>
2023-02-02 14:27:30 -0500

    started to create person comp

--- src/PeopleList.js
+++ src/PeopleList.js
@@ -1,51 +1,55 @@
-export function PeopleList() {
-    let persons = [];
-    let url = 'https://swapi.dev/api/people';
-    console.log(fetch(url));
+import { useEffect, useState } from "react";
 
-    // const getPersonIdFromUrl = (url) => {
-    //     const re = /.*people\/(\d+).*/
-    //     const matches = url.match(re)
-    //     if (!matches) throw "Bad URL. Not a people URL."
-    //     return matches[1]
-    //   }
+export function PeopleList() {
+  const [persons, setPersons] = useState([]);
+  let dummyPersons = [];  
+
+  let url = 'https://swapi.dev/api/people';
+
+    useEffect(() => {getPeople()}, [])
+
     async function getPeople(){
-        while (url) {
-            try {
-              const fetchedPersons = await fetch(url)
-                .then(res => res.json())
-                .then(res => { url = res.next; return res })
-                .then(res => res.results)
-                // .then(res => res.map(p => ({ ...p, id: +getPersonIdFromUrl(p.url) })))
-              persons.push(...fetchedPersons);
-            }
-            catch (ex) {
-              console.error("Error reading people.", ex.message);
-            }
-          }
-          console.log("All the persons are ", persons)
+      while (url) {
+        try {
+          const fetchedPersons = await fetch(url)
+            .then(res => res.json())
+            .then(res => { url = res.next; return res })
+            .then(res => res.results)
+            .then(res => res.map(p => ({ ...p, id: +getPersonIdFromUrl(p.url) })))
+            dummyPersons.push(...fetchedPersons);
+        }
+        catch (ex) {
+          console.error("Error reading people.", ex.message);
+        }
+        }
+        setPersons(dummyPersons)
     }
 
-    getPeople();
-   
-
-      
-
+    const getPersonIdFromUrl = (url) => {
+      const re = /.*people\/(\d+).*/
+      const matches = url.match(re)
+      if (!matches) throw "Bad URL. Not a people URL."
+      return matches[1]
+    }
+    
+    console.log("All the persons are ", persons)
       return (
         <>
-            <div>
-    <h1>Star Wars Universe Lookup</h1>
-    
-    <label for="searchString">Who you looking for? <span class="small">(Regular expressions are cool
-        here)</span></label>
-    <input id="searchString"  autocomplete="off" />
-  </div>
-  <section id="people">
-        {persons.map(person => <div>
-            {person.name}
-        </div>)}
-  </section>
+          <div>
+            <h1>Star Wars Universe Lookup</h1>
+            
+            <label for="searchString">Who you looking for? <span className="small">(Regular expressions are cool
+                here)</span></label>
+            <input id="searchString"  autoComplete="off" />
+          </div>
 
+          <section id="people">
+            {persons.length > 0 && 
+            persons.map(person => <div>
+                {person.name}
+            </div>)
+            }
+          </section>
         </>
       )
     }
