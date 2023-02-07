4d7d2f5773d3c7f4e84b2cf755a3a72cb5828d92
Bob Smith <wasadmin@webagesolutions.com>
2023-02-07 10:38:53 -0500

    minor changes

--- src/PersonPage.js
+++ src/PersonPage.js
@@ -34,16 +34,17 @@ export function PersonPage() {
         <>
             <h1 id="name"></h1>
             <section id="generalInfo">
+                <h2>{foundPerson.name}</h2>
                 <p>Height: <span id="height">{foundPerson.height}</span> cm</p>
                 <p>Mass: <span id="mass">{foundPerson.mass}</span> kg</p>
                 <p>Born: <span id="birth_year">{foundPerson.birth_year}</span></p>
             </section>
             <section id="planets">
-                <h2>Homeworld</h2>
+                <h3>Homeworld</h3>
                 <p><span id="homeworld"></span></p>
             </section>
             <section id="films">
-                <h2>Films appeared in</h2>
+                <h3>Films appeared in</h3>
                 <ul></ul>
             </section>
         </>
