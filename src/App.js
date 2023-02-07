c4abef18dc4580f3507cdb2772eade0f10d13409
Bob Smith <wasadmin@webagesolutions.com>
2023-02-07 10:54:44 -0500

    rendering info to lanet page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { PeopleList } from './PeopleList';
 import {Route, Routes} from "react-router-dom"; 
 import { PersonPage } from './PersonPage';
+import { PlanetPage } from './PlanetPage';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
        <Route path="/" element={<PeopleList />} />
        <Route path="/person" element={<PersonPage />} />
        <Route path="/person/:id" element={<PersonPage />} />
+       <Route path="/planet" element={<PlanetPage />} />
+       <Route path="/planet/:id" element={<PlanetPage />} />
     </Routes>
     </> 
     );
