dcae8bebde352e0d8998934f2e196e5eb2e3b5b6
Bob Smith <wasadmin@webagesolutions.com>
2023-02-08 15:08:55 -0500

    server is running

--- hackathon/server/server.js
+++ hackathon/server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
-app.use(cors());
-app.get('/', (req, res) => res.send('Home Route'));
+app.use(express());
+app.get('/', (req, res) => res.send('The server is running:)'));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}, http://localhost:${port}`));
