8eb478651be104e5cdd428e761fd40382f0ed57b
Bob Smith <wasadmin@webagesolutions.com>
2023-02-08 16:57:36 -0500

    added router

--- hackathon/client/src/components/NavBar.js
+++ hackathon/client/src/components/NavBar.js
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export function NavBar() {
     return(
         <>
+            <Link to="/">
             <span>
                 Home
             </span>
+            </Link>
             <span>
                 Login
             </span>
